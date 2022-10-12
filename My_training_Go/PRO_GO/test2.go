02e3aadcb0ebc57e4bd43c408c03c0eb790f8582
shindos <shindos@internet.ru>
2022-10-12 22:02:03 +0300

    str.228

--- My_training_Go/PRO_GO/test2.go
+++ My_training_Go/PRO_GO/test2.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Println(sum)
+	products := [4]string{"Kayak", "Lifejacket", "Paddle", "Hat"}
+	allNames := products[1:]
+	someNames := allNames[1:3]
+	allNames = append(allNames, "Gloves")
+	allNames[1] = "Canoe"
+	fmt.Println("someNames:", someNames)
+	fmt.Println("allNames", allNames)
 }
