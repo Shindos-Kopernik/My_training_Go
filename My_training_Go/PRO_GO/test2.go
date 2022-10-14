2b2abbf6e1902f5abe9dd4ecd14afbff9cab1cfe
shindos <shindos@internet.ru>
2022-10-14 21:50:41 +0300

    str.261

--- My_training_Go/PRO_GO/test2.go
+++ My_training_Go/PRO_GO/test2.go
@@ -2,18 +2,13 @@ package main
 
 import "fmt"
 
+func printPrice() {
+	kayakPrice := 275.00
+	kayakTax := kayakPrice * 0.2
+	fmt.Println("Price:", kayakPrice, "Tax:", kayakTax)
+}
 func main() {
-	product := map[string]float64{
-		"Kayak": 1,
-		"Lol":   40.12,
-		"Hat":   0,
-	}
-	value, ok := product["Hat"]
-	if ok {
-		fmt.Println("Stored value:", value)
-	} else {
-		fmt.Println("No")
-	}
-	fmt.Println(product["Kayak"])
-	fmt.Println(product["Lol"])
+	fmt.Println("About to call function")
+	printPrice()
+	fmt.Println("Function complete")
 }
