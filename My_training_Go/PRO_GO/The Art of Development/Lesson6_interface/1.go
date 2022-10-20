9c7156d1e10c55c1b6900f13a028bc6d75e4b883
shindos <shindos@internet.ru>
2022-10-20 21:54:22 +0300

    Art main2.go

--- My_training_Go/PRO_GO/The Art of Development/Lesson6_interface/1.go
+++ My_training_Go/PRO_GO/The Art of Development/Lesson6_interface/1.go
@@ -1,22 +1,34 @@
 package main
 
+import "fmt"
+
 type structHere struct {
 	N1, N2 int
 }
 
-func (s *structHere) Sum() {
+func (s *structHere) Sum() int {
 	return s.N1 + s.N2
 }
 
-type Interface interface {
+type InterfaceHere interface {
 	Sum() int
 }
 
-func main()  {
-
+func main() {
+	var a InterfaceHere
+	sh := structHere{N1: 1, N2: 2}
+	os := otherStruct{A: 2, B: 3}
+	a = &sh
+	a.Sum()
+	fmt.Println(a.Sum())
+	a = &os
+	fmt.Println(a.Sum())
+	var i InterfaceHere = otherStruct{A: 10, B: 43}
+	fmt.Println(i.Sum())
 }
+
 type otherStruct struct {
-	A, B int()
+	A, B int
 }
 
 func (o otherStruct) Sum() int {
