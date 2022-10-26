ebc7e6c34d9489ffed7024d3e75f66c5bb709016
shindos <shindos@internet.ru>
2022-10-26 15:54:38 +0300

    Add file to folder Les6, todo Lesson7

--- My_training_Go/PRO_GO/The Art of Development/Lesson6_interface/main2.go
+++ My_training_Go/PRO_GO/The Art of Development/Lesson6_interface/main2.go
@@ -1,1 +1,37 @@
 package main
+
+var _ User = &superUser{}
+
+type superUser struct {
+	Name      string
+	Age       int
+	IsBlocked bool
+}
+
+func (s *superUser) Block() {
+	u.IsBlocked = true
+}
+
+var _ User = &user{}
+
+type user struct {
+	FIO, Address, Phone string
+	IsBlocked           bool
+}
+
+func (u *user) Block() {
+	u.IsBlocked = true
+}
+
+type User interface {
+	Block()
+}
+
+func NewUser(fio, address, phone string) User {
+	u := user{}
+	return &u
+}
+func main() {
+	u := NewUser("", "", "")
+	u.Block()
+}
