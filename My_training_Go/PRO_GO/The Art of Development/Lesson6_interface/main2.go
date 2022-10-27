b0d3b72e3a3a4f5b94596799e92cd63380c1cf62
shindos <shindos@internet.ru>
2022-10-27 21:21:08 +0300

    Art main2.go

--- My_training_Go/PRO_GO/The Art of Development/Lesson6_interface/main2.go
+++ My_training_Go/PRO_GO/The Art of Development/Lesson6_interface/main2.go
@@ -1,1 +1,48 @@
 package main
+
+var _ User = superUser{}
+
+type superUser struct {
+	Name string
+	Age  int
+}
+
+func (s *superUser) ChangeFIO(newFIO string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *superUser) ChangeAddress(newAddress string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+var _ User = &user{}
+
+type user struct {
+	FIO, Address, Phone string
+}
+
+func (u *user) ChangeFIO(newFIO string) {
+	u.FIO = newFIO
+}
+
+func (u *user) ChangeAddress(newAddress string) {
+	u.Address = newAddress
+}
+
+type User interface {
+	ChangeFIO(newFIO string)
+	ChangeAddress(newAddress string)
+}
+
+func NewUser(fio, address, phone string) {
+	u := user{
+		FIO:     fio,
+		Address: address,
+		Phone:   phone,
+	}
+}
+func main() {
+
+}
