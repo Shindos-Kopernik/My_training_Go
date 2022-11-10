0a2a33a820804fc1d6f37163f47c3da83d0c5815
shindos <shindos@internet.ru>
2022-11-11 00:11:58 +0300

    Les3 25_40

--- My_training_Go/The Art of Development/Lessons_Golang2_Advanced_Playlist2/Lesson1_Rest_API/pkg/logging/logging.go
+++ My_training_Go/The Art of Development/Lessons_Golang2_Advanced_Playlist2/Lesson1_Rest_API/pkg/logging/logging.go
@@ -35,12 +35,12 @@ type Logger struct { // своя структура т.к. логгер може
 	*logrus.Entry
 }
 
-func GetLogger() Logger {
-	return Logger{e}
+func GetLogger() *Logger {
+	return &Logger{e}
 }
 
-func (l *Logger) GetLoggerWithField(k string, v interface{}) Logger {
-	return Logger{l.WithField(k, v)}
+func (l *Logger) GetLoggerWithField(k string, v interface{}) *Logger {
+	return &Logger{l.WithField(k, v)}
 }
 
 func init() {
