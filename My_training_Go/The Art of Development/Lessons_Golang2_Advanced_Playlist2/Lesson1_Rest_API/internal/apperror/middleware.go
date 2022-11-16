b21bffe3be5dd6ed6b56144eb158c4f6e2890994
shindos <shindos@internet.ru>
2022-11-16 21:47:45 +0300

    edit middleware.go

--- My_training_Go/The Art of Development/Lessons_Golang2_Advanced_Playlist2/Lesson1_Rest_API/internal/apperror/middleware.go
+++ My_training_Go/The Art of Development/Lessons_Golang2_Advanced_Playlist2/Lesson1_Rest_API/internal/apperror/middleware.go
@@ -1,1 +1,35 @@
 package apperror
+
+import (
+	"errors"
+	"net/http"
+)
+
+type appHandler func(w http.ResponseWriter, r *http.Request) error
+
+func Middleware(h appHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var appErr *AppError
+		err := h(w, r)
+		if err != nil {
+			if errors.As(err, &appErr) {
+				if errors.Is(err, ErrNotFound) {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write(ErrNotFound.Marshal())
+					return
+				}
+				//} else if errors.Is(err, NoAuthErr) {
+				//	w.WriteHeader(http.StatusUnauthorized)
+				//	w.Write(ErrNotFound.Marshal())
+				//	return
+				//}
+				err = err.(*AppError)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write(ErrNotFound.Marshal())
+			}
+
+			w.WriteHeader(http.StatusTeapot) //не наша ошибка, отдаем как есть. А надо обернуть в систем. ошибку
+			w.Write([]byte(err.Error()))
+		}
+	}
+}
--- My_training_Go/The Art of Development/Lessons_Golang2_Advanced_Playlist2/Lesson1_Rest_API/internal/user/handler.go
+++ My_training_Go/The Art of Development/Lessons_Golang2_Advanced_Playlist2/Lesson1_Rest_API/internal/user/handler.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"Lesson1_Rest_API/internal/apperror"
 	"Lesson1_Rest_API/internal/handlers"
 	"Lesson1_Rest_API/pkg/logging"
 	"github.com/julienschmidt/httprouter"
@@ -25,36 +26,48 @@ func NewHandler(logger *logging.Logger) handlers.Handler {
 
 }
 func (h *handler) Register(router *httprouter.Router) {
-	router.GET(usersURL, h.GetList)
-	router.POST(usersURL, h.CreateUser)
-	router.GET(userURL, h.GetUserByUUID)
-	router.PUT(userURL, h.UpdateUser)
-	router.PATCH(userURL, h.PartiallyUpdateUser)
-	router.DELETE(userURL, h.DeleteUser)
+	router.HandlerFunc(http.MethodGet, usersURL, apperror.Middleware(h.GetList))
+	router.HandlerFunc(http.MethodPost, usersURL, apperror.Middleware(h.CreateUser))
+	router.HandlerFunc(http.MethodGet, userURL, apperror.Middleware(h.GetUserByUUID))
+	router.HandlerFunc(http.MethodPut, userURL, apperror.Middleware(h.UpdateUser))
+	router.HandlerFunc(http.MethodPatch, userURL, apperror.Middleware(h.PartiallyUpdateUser))
+	router.HandlerFunc(http.MethodDelete, userURL, apperror.Middleware(h.DeleteUser))
 
 }
-func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(200)
 	w.Write([]byte("this is list of users"))
+
+	return nil
 }
 
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(201)
 	w.Write([]byte("this is create user"))
+
+	return nil
 }
-func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(200)
 	w.Write([]byte("this is user by uuid"))
+
+	return nil
 }
-func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("this is update users"))
+
+	return nil
 }
-func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("this is partially update users"))
+
+	return nil
 }
-func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("this is delete users"))
+
+	return nil
 }
