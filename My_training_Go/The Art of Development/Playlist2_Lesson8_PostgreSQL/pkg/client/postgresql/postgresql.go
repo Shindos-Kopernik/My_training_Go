de710130adf8c5a57201fba6fd627f3b75ebdd10
shindos <shindos@internet.ru>
2022-11-21 16:21:21 +0300

    28-49 PSQL_Part1

--- My_training_Go/The Art of Development/Playlist2_Lesson8_PostgreSQL/pkg/client/postgresql/postgresql.go
+++ My_training_Go/The Art of Development/Playlist2_Lesson8_PostgreSQL/pkg/client/postgresql/postgresql.go
@@ -1,6 +1,7 @@
 package postgresql
 
 import (
+	"Lesson1_Rest_API/internal/config"
 	"Lesson1_Rest_API/pkg/utils"
 	"context"
 	"fmt"
@@ -20,13 +21,13 @@ type Client interface {
 	Begin(ctx context.Context) (pgx.Tx, error)
 }
 
-func NewClient(ctx context.Context, maxAttempts int, username, password, host, port, database string(*pgxpool.Pool, err error) {
-	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", username, password, host, port, database)
-	err := utils.DoWithTries(func() error {
+func NewClient(ctx context.Context, maxAttempts int, sc config.StorageConfig) (pool *pgxpool.Pool, err error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", sc.Username, sc.Password, sc.Host, sc.Port, sc.Database)
+	err = utils.DoWithTries(func() error {
 		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 		defer cancel()
 
-		pool, err := pgxpool.Connect(ctx, dsn)
+		pool, err = pgxpool.Connect(ctx, dsn)
 		if err != nil {
 			return err
 		}
